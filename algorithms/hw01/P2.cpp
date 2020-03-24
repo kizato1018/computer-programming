c46667e7383e143fa9a90981bef618eeb2ed83e2
kizato1018 <kizato1018@gmail.com>
2020-03-24 14:11:03 +0800

    2020/3/24 Update

--- algorithms/hw01/P2.cpp
+++ algorithms/hw01/P2.cpp
@@ -9,17 +9,15 @@ int main() {
 
 	int n, d;
 	cin >> n >> d;
-	int a[n], b[n+1];
+	int a[n], c = 0;
 	bool flag = false;
-	for(size_t i = 0; i < n; i++)
+	for(size_t i = 0; i < n; i++) {
 		cin >> a[i];
-	b[0] = 0;
-	for(size_t i = 1; i <= n; i++)
-		b[i] = b[i-1] + a[n-i];
-	for(size_t i = 1; i <= n; i++) {
-		for(size_t j = 0; j < i; j++) {
-			if(b[i] - b[j] == d) flag = true;
-			else if(b[i] - b[j] < d) break;
+		if(flag) continue;
+		for(int j = c; j < i; j++) {
+			a[j] += a[i];
+			if(a[j] > d) c++;
+			else if(a[j] == d) flag = true;
 		}
 	}
 	if(flag) cout << "yes" << endl;
