14c952198cc886ca3c0a2155d5447b40fae3a0b7
kizato1018 <kizato1018@gmail.com>
2020-04-10 10:54:30 +0800

    2020/4/10 Update

--- algorithms/hw02/P1.cpp
+++ algorithms/hw02/P1.cpp
@@ -7,13 +7,19 @@ using namespace std;
 
 int main() {
     int n;
-    int64_t arr[2][N] = {};
-    int Max = 0;
+    int64_t arr[N] = {};
+    int64_t fin[N] = {};
+    int64_t Max = 0;
     scanf("%d", &n);
     for(int i = 0; i < n; i++) {
-        scanf("%ld", &arr[0][i]);
+        scanf("%ld", &arr[i]);
+        fin[i] = arr[i];
     }
-    printf("%d\n", Max);
+    sort(fin, fin+n);
+    for(int i = n-1; i >= 0; i--) {
+        Max = max(Max, i-(find(fin, fin+i, arr[i])-fin));
+    }
+    printf("%ld\n", Max);
 }
 // 10
 // 89 7 22 35 4 11 23 90 55 64
