eaebcd61c6a9b1adc39b0f5d0022b88c0b26db50
kizato1018 <kizato1018@gmail.com>
2020-06-15 16:19:49 +0800

    CP2 practice linklist

--- CP2/practice/linklist.c
+++ CP2/practice/linklist.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _List {
+
+typedef struct _Node {
     int value;
-    struct _List* next;
+    struct _Node* next;
+}Node;
+
+typedef struct _List {
+    int size;
+    Node* head;
+    Node* tail;
 }List;
 
-void add(List* L, int new) {
-    List *n = calloc(1, sizeof(List));
+
+void add(List* Head, int new) {
+    if(Head == NULL) return;
+    Node *n = calloc(1, sizeof(List));
     n->value = new;
     n->next = NULL;
-    if(L == NULL) 
-        L = n;
-    else
-        L->next = n;
+    if(Head->size == 0) {
+        Head->head = n;
+        Head->tail = n;
+        ++Head->size;
+    }
+    else{
+        Node *cur = Head->tail;
+        cur->next = n;
+        Head->tail = n;
+        ++Head->size;
+    }
+    // if(L == NULL) 
+    //     L = n;
+    // else
+    // while(cur->next != NULL)
+    //     cur = cur->next;
+    // cur->next = n;
     return;
 }
 
-void delete(List* L, int val) {
-    List* cur = L;
-    if(cur == NULL) return;
-    List* next = cur->next;
-    if(cur->value == val) {
-        L = next;
+void delete(List* Head, int val) {
+    if(Head == NULL || Head->size == 0) return;
+    Node* cur = Head->head;
+    Node* next = cur->next;
+    while(cur->value == val) {
+        Head->head = next;
         free(cur);
-        cur = L;
-        next = next->next;
+        --Head->size;
+        if(Head->head == NULL) return;
+        cur = Head->head;
+        next = cur->next;
     }  
-
-    while(next) {
+    while(next != NULL) {
         if(next->value == val) {
-            cur = next->next;
+            --Head->size;
+            if(next == Head->tail) {
+                Head->tail = cur;
+                cur->next = NULL;
+                free(next);
+                return;
+            }
+            cur->next = next->next;
             free(next);
             next = cur->next;
         }
@@ -42,8 +72,38 @@ void delete(List* L, int val) {
 
 }
 
-int main() {
-    List l;
-    int ll[100][2] = {{30,1}, {40, 2}, {50, -1}};
+void clear(List* Head) {
+    Node* cur = Head->head;
+    while(cur != NULL) {
+        Head->head = cur->next;
+        free(cur);
+        cur = Head->head;
+        --Head->size;   
+    }
+    Head->tail = NULL;
+}
 
+void Print(List *Head) {
+    if(Head == NULL) return;
+    Node* cur = Head->head;
+    while(cur != NULL) {
+        printf("%d --> ", cur->value);
+        cur = cur->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    List Head = {0, NULL};
+    add(&Head, 10);
+    add(&Head, 10);
+    add(&Head, 20);
+    add(&Head, 30);
+    add(&Head, 30);
+    add(&Head, 15);
+    Print(&Head);
+    delete(&Head, 10);
+    Print(&Head);
+    clear(&Head);
+    Print(&Head);
 }
